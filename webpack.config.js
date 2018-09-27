fbe491054c4a981c50a1c4ea4e2543c63d5c7b01
unknown <ramaiahrajg@gmail.com>
2018-09-27 18:19:53 +0530

    DIST

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/dist/'
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
