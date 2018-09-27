9adef0f87739ae8a0f0105fd8d6e7703a00de66a
unknown <ramaiahrajg@gmail.com>
2018-09-27 18:00:36 +0530

    WebPack Babel Bootstrap config

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,44 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080
+  },
+  devtool : "source-maps",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+           
+          }
+        }
+      },
+      {
+        test: /\.(scss)$/,
+        use: [{
+              loader: 'style-loader' // inject CSS to page
+               
+              }, {
+              loader: 'css-loader' // translates CSS into CommonJS modules
+               
+              },
+              {
+              loader: 'sass-loader' ,// compiles Sass to CSS
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+    
+    }
+  ]
   }
 };
